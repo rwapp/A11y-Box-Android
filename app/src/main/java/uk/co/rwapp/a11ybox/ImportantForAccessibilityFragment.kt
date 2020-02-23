b0bbf725ab31f05678fa31a51b169e562b4dcd85
rwapp <r.whitaker@mac.com>
2020-02-23 17:22:18 +0000

    remove unneeded comments

--- app/src/main/java/uk/co/rwapp/a11ybox/ImportantForAccessibilityFragment.kt
+++ app/src/main/java/uk/co/rwapp/a11ybox/ImportantForAccessibilityFragment.kt
@@ -14,7 +14,6 @@ class ImportantForAccessibilityFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_important, container, false)
     }
 
